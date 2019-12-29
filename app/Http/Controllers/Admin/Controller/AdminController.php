e1a696e41d194397fc8e8c74874233d1c67304e6
lchtechhk <lchtechhk@gmail.com>
2019-12-29 17:54:37 +0800

    update library

---
+++ app/Http/Controllers/Admin/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Controller;
+use App\Http\Controllers\Controller;
+// use Validator;
+// use App;
+// use Lang;
+use Log;
+// use DB;
+// use Hash;
+// use Auth;
+// use Session;
+// use Illuminate\Http\Request;
+
+
+class AdminController extends Controller{
+
+	function __construct(){
+
+	}
+	public function login(){
+		// if (Auth::check()) {
+		//   return redirect('/admin/dashboard/this_month');
+		// }else{
+		// 	$title = array('pageTitle' => Lang::get("labels.login_page_name"));
+		// 	return view("admin.login",$title);
+		// }
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
+header("Cache-Control: no-cache, must-revalidate");
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function() { return Redirect::to("admin/login"); });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Admin', 'prefix'=>'admin'], function () {
+    // Route::get('/login', 'AdminController@login');
+    Route::get('/login', function () {
+        return view('welcome');
+    });
+
 });
