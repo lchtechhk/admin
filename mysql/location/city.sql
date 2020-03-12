365ea5b5ec0de7a4dd9309b5d6ceff04d55b0b5d
DESKTOP-HQI4FAG\jamie <jamielamproject>
2020-03-12 12:12:48 +0800

    mysql

---
+++ mysql/location/city.sql
@@ -0,0 +1,16 @@
+INSERT INTO `cms`.`cities` 
+(`id`, `company_id`, `name`, `code`, `countries_id`, `create_date`, `edit_date`, `status`) 
+VALUES 
+('1', '10', '港島', 'HONG KONG', '1', '2020-03-12 03:05:42', '2020-03-12 03:05:42', 'active'),
+('2', '10', '九龍', 'KOWLOON', '1', '2020-03-12 03:05:42', '2020-03-12 03:05:42', 'active'),
+('3', '10', '新界', 'NEW ERRITORIES', '1', '2020-03-12 03:05:42', '2020-03-12 03:05:42', 'active')
+;
+
+
+INSERT INTO `cms`.`district` (`id`, `company_id`, `name`, `code`, `area_id`, `create_date`, `edit_date`, `status`) 
+VALUES 
+('1', '10', '堅尼地城', 'Kennedy Town', '1', '2020-03-12 02:27:40', '2020-03-12 02:27:40', 'active'),
+('2', '10', '石塘咀', 'Shek Tong Tsui', '1', '2020-03-12 02:27:40', '2020-03-12 02:27:40', 'active'),
+('3', '10', '西營盤', 'Sai Ying Pun', '1', '2020-03-12 02:27:40', '2020-03-12 02:27:40', 'active')
+
+;
