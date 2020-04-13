5333e06c9b42b286fcbc31e695178495f8de14b7
lchtechhk <lchtechhk@gmail.com>
2020-04-14 02:12:15 +0800

    http to https

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Log;
+
+class HttpsProtocol {
+
+    public function handle($request, Closure $next)
+    {
+        Log::info("HttpsProtocol : " . $request->getRequestUri());
+        if (!$request->secure()) {
+            // if (!$request->secure() && App::environment() === 'production') {
+                // return redirect()->secure('/');
+                return redirect()->secure($request->getRequestUri());
+            }
+
+            return $next($request); 
+    }
+}
